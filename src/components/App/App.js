59d46208dc9177d2c7140a35c994149eb7162acb
Tvister <0140@rambler.ru>
2018-12-22 13:07:32 +0500

    Creat lab2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ChristmasTree from '../ChristmasTree/ChristmasTree';
+import Labyrinth from '../Labyrinth/Labyrinth';
+
 import styles from './style.css';
 
 export default class App extends React.Component {
@@ -8,6 +10,9 @@ export default class App extends React.Component {
             <div className={styles.app}>
                 <div className={styles.treeWrapper}>
                     <ChristmasTree />
+                </div>
+                <div className={styles.labyrinthWrapper}>
+                    <Labyrinth />
                 </div>
             </div>
         );
